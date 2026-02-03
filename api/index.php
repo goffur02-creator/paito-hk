f47ed4229097e2d7abd0562c7ea20bcdab7328fe
goffur02-creator <goffur02@gmail.com>
2026-02-03 19:58:55 +0700

    fix syntax error

--- api/index.php
+++ api/index.php
@@ -1,4 +1,17 @@
 <?php
 header("Content-Type: application/json");
-$data = ["status" => "online", "source" => "HK Paito", "numbers" => [1234, 5678, 9012]];
-echo json_encode($data);
+$target_url = "https://www.dlhntb.org"; 
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $target_url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0");
+$html = curl_exec($ch);
+curl_close($ch);
+preg_match_all('/\b\d{6}\b/', $html, $matches);
+$data_6d = array_values(array_unique($matches[0]));
+echo json_encode([
+    "status" => "success",
+    "type" => "HK 6D",
+    "last_result" => $data_6d[0] ?? "N/A",
+    "paito_list" => array_slice($data_6d, 0, 30)
+]);
